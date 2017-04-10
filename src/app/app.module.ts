75b0412ea5ec97b9511dab8c9435d6221a427434
Narendra Pathai <narendra.pathai@gmail.com>
2017-04-10 16:58:15 +0530

    Added autofocus on editable task when edit mode is enabled on clicking the task label. Added a directive that does this

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
 import { TaskComponent } from './todo/task.component';
+import { FocusDirective } from './directive/focus.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoComponent,
-    TaskComponent
+    TaskComponent,
+    FocusDirective
   ],
   imports: [
     BrowserModule,
